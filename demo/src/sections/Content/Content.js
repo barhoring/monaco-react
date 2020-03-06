bcf095c74d42535e537b16f8e1c85922d65e0d1e
Bar Horing Amir <bar.apple@gmail.com>
2020-03-06 19:41:57 +0200

    menu file choser in Diff Editor

--- demo/src/sections/Content/Content.js
+++ demo/src/sections/Content/Content.js
@@ -1,10 +1,17 @@
 import React from "react";
 
+import TextField from "@material-ui/core/TextField";
+import MenuItem from "@material-ui/core/MenuItem";
+
+import config from "config";
+
 import Paper from "@material-ui/core/Paper";
 import Divider from "@material-ui/core/Divider";
 import Typography from "@material-ui/core/Typography";
 
-// import Editor from 'sections/Editor';
+import Editor from "sections/Editor";
+import Settings from "../Settings";
+
 import DiffEditor from "sections/DiffEditor";
 
 import classNames from "classnames";
@@ -19,6 +26,8 @@ const Content = _ => {
       square={true}
       className={classNames("full-size", classes.root)}
     >
+      <Settings />
+
       <div className={classes.editor}>
         <Typography variant="h4" className={classes.title}>
           Editor
@@ -31,6 +40,25 @@ const Content = _ => {
           Diff Editor
         </Typography>
         <Divider />
+        <TextField
+          select
+          variant="filled"
+          // value={monacoTheme}
+          onChange={ev => {
+            console.log("changed!");
+            console.log(this);
+            console.log(ev);
+          }}
+          className="full-width"
+          label="File"
+          // label="Theme"
+        >
+          {config.monacoThemes2.map(theme => (
+            <MenuItem key={theme} value={theme}>
+              {theme}
+            </MenuItem>
+          ))}
+        </TextField>
         <DiffEditor />
       </div>
     </Paper>
