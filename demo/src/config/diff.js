31c8992993e09816902a81ae7febbddd3354c03e
Bar Horing Amir <bar.apple@gmail.com>
2020-03-06 15:53:04 +0200

    first commit in

--- demo/src/config/diff.js
+++ demo/src/config/diff.js
@@ -1,179 +1,14 @@
 /* eslint-disable */
-import { rTabs } from 'utils';
+import { rTabs } from "utils";
 
 const examples = {
   original: rTabs(`
-    # monaco-react
-    Monaco Editor for React
-
-    [![NPM](https://nodei.co/npm/@monaco-editor/react.png.png)](https://nodei.co/npm/@monaco-editor/react/)
-
-    ## Synopsis
-
-    Monaco editor wrapper for easy/one-line integration with React applications without need of webpack (or other module bundler) configuration files.
-
-    ## Motivation
-    There is a well-known web technology based code editor called [Monaco Editor](https://microsoft.github.io/monaco-editor/) that powers [VS Code](https://code.visualstudio.com/). [There are also many ways to integrate](https://github.com/Microsoft/monaco-editor-samples/) it provided by monaco creators. But there were tons of problems with integration of monaco with modern technologies; e.g React.
-
-    There also exist solutions for integration with React; e.g [this one](https://github.com/react-monaco-editor/react-monaco-editor) and [this one](https://github.com/jaywcjlove/react-monacoeditor). But they need some custom webpack configuration to make Monaco fully work, which is not the "best" solution for such kind of things like create-react-app - [CRA](https://facebook.github.io/create-react-app/).
-
-    With this solution, you don't need any kind of webpack configuration files and it works great with React apps created by CRA or created by something else.
-
-    ## Installation
-
-    \`\`\`bash
-    yarn add @monaco-editor/react # or npm install @monaco-editor/react
-    \`\`\`
-
-    ## [Demo](https://monaco-react.surenatoyan.com/)
-
-    ## Usage
-
-    Example ([codesandbox](https://codesandbox.io/s/monaco-editor-react-u0fyv))
-    \`\`\`js
-    import React, { useState } from 'react';
-    import Editor from '@monaco-editor/react';
-
-    const examples = { python: '# python example', javascript: '// javascript example' };
-
-    function App() {
-      const [theme, setTheme] = useState('light');
-      const [language, setLanguage] = useState('javascript');
-      const [isEditorReady, setIsEditorReady] = useState(false);
-
-      function handleEditorDidMount() {
-        setIsEditorReady(true);
-      }
-      
-      function toggleTheme() {
-        setTheme(theme === 'light' ? 'dark' : 'light');
-      }
-      
-      function toggleLanguage() {
-        setLanguage(language === 'javascript' ? 'python' : 'javascript');
-      }
-
-      return (
-        <>
-          <button onClick={toggleTheme} disabled={!isEditorReady}>Toggle theme</button>
-          <button onClick={toggleLanguage} disabled={!isEditorReady}>Toggle language</button>
-
-          <Editor
-            height={500} // By default, it fully fits with its parent
-            theme={theme}
-            language={language}
-            value={examples[language]}
-            editorDidMount={handleEditorDidMount}
-            loading={'Loading...'}
-          />
-        </>
-      );
-    }
-
-    export default App;
-    \`\`\`
-
-    #### MonacoEditor instance
-
-    The second argument of \`editorDidMount\` is the instance of the editor. So, you can use it to get the full control on the editor if you need it.
-
-    ## Props
-
-    #### Editor
-
-    | Name   |      Type      |  Default |  Description |
-    |:----------|:-------------|:------|:------|
-    | value | string || The editor value |
-    | language | enum: ... | | All languages that are [supported](https://github.com/microsoft/monaco-languages) by monaco-editor |
-    | editorDidMount | func | noop | **Signature: function(getEditorValue: func, monaco: object) => void** <br/> This function will be called right after monaco editor will be mounted and ready to work. It will get the editor instance as a second argument |
-    | theme | enum: 'light' \| 'dark' | 'light' | Default themes of monaco |
-    | line | number |  | The line to jump on it |
-    | width | union: number \| string | '100%' | The width of the editor wrapper |
-    | height | union: number \| string | '100%' | The height of the editor wrapper |
-    | loading | union: React element \| string | 'Loading...' | The loading screen before the editor will be loaded |
-    | options | object | {} | [IEditorOptions](https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.ieditoroptions.html) |
-
-    #### DiffEditor
-
-    | Name   |      Type      |  Default |  Description |
-    |:----------|:-------------|:------|:------|
-    | original | string || The original source (left one) value |
-    | modified | string || The modified source (right one) value |
-    | language | enum: ... | | All languages that are [supported](https://github.com/microsoft/monaco-languages) by monaco-editor |
-    | originalLanguage | enum: ... | *language | This prop gives you the opportunity to specify the language of the \`original\` source separately, otherwise, it will get the value of \`language\` property. (Possible values are the same as \`language\`) |
-    | modifiedLanguage | enum: ... | *language | This prop gives you the opportunity to specify the language of the \`modified\` source separately, otherwise, it will get the value of \`language\` property. (Possible values are the same as \`language\`) |
-    | editorDidMount | func | noop | **Signature: function(getOriginalEditorValue: func, getModifiedEditorValue: func, monaco: object) => void** <br/> This function will be called right after monaco editor will be mounted and ready to work. It will get the editor instance as a third argument |
-    | theme | enum: 'light' \| 'dark' | 'light' | Default themes of monaco |
-    | line | number |  | The line to jump on it |
-    | width | union: number \| string | '100%' | The width of the editor wrapper |
-    | height | union: number \| string | '100%' | The height of the editor wrapper |
-    | loading | union: React element \| string | 'Loading...' | The loading screen before the editor will be loaded |
-    | options | object | {} | [IDiffEditorOptions](https://microsoft.github.io/monaco-editor/api/interfaces/monaco.editor.idiffeditorconstructionoptions.html) |
-
-    ## License
-
-    [MIT](./LICENSE)
+  <!DOCTYPE html>\r\n<html lang=\"en\">\r\n  <head>\r\n    <meta charset=\"UTF-8\" />\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\" />\r\n    <title>Document</title>\r\n  </head>\r\n  <body>\r\n    <div id=\"root\">not rendered</div>\r\n    <script src=\"https://unpkg.com/react@16.4.1/umd/react.development.js\"></script>\r\n    <script src=\"https://unpkg.com/react-dom@16.4.1/umd/react-dom.development.js\"></script>\r\n  </body>\r\n</html>\r\n
   `),
   modified: rTabs(`
-    # monaco-react
-    Monaco Editor for React
-
-    [![NPM](https://nodei.co/npm/@monaco-editor/react.png.png)](https://nodei.co/npm/@monaco-editor/react/)
-
-    ## Synopsis
-
-    Monaco editor wrapper for easy/one-line integration with React applications without need of webpack (or other module bundler) configuration files.
-
-    ## Motivation
-    There is a well-known web technology based code editor called [Monaco Editor](https://microsoft.github.io/monaco-editor/) that powers [VS Code](https://code.visualstudio.com/). [There are also many ways to integrate](https://github.com/Microsoft/monaco-editor-samples/) it provided by monaco creators. But there were tons of problems with integration of monaco with modern technologies; e.g React.
-
-    There also exist solutions for integration with React; e.g [this one](https://github.com/react-monaco-editor/react-monaco-editor) and [this one](https://github.com/jaywcjlove/react-monacoeditor). But they need some custom webpack configuration to make Monaco fully work, which is not the "best" solution for such kind of things like create-react-app - [CRA](https://facebook.github.io/create-react-app/).
-
-    **With this solution, you don't need any kind of webpack configuration files and it works great with React apps created by CRA or created by something else.**
-
-    #### Demo
-    [Check it out!](https://monaco-react.surenatoyan.com/)
-
-    ## Documentation
-
-    #### Contents
-
-    * [Installation](#installation)
-    * [Introduction](#introduction)
-    * Usage
-      * [Simple Usage](#simple-usage)
-      * [Get Value](#get-value)
-      * [Monaco Instance](#monaco-instance)
-      * [Editor Instance](#editor-instance)
-      * [Controlled Editor](#controlled-editor)
-    * Props
-      * [Editor](#editor)
-      * [Diff Editor](#diffeditor)
-      * [Controlled Editor](#controlled-editor) 
-
-    #### Installation
-
-    \`\`\`bash
-    yarn add @monaco-editor/react # or npm install @monaco-editor/react
-    \`\`\`
-
-    #### Introduction
-
-    Besides types three main components are also exported from the package:
-
-    * Editor
-    * DiffEditor
-    * ControlledEditor
-
-    And the utility that gives ability to access to monaco instance (simply called "monaco")
-
-    #### Simple Usage
-
-    Here is an example of a simple integration of monaco editor with a react project.
-    You just need to import and render the Editor component.
-    You can play with it [here](https://codesandbox.io/s/dreamy-lumiere-c8pib?fontsize=14)
-
-    \`\`\`js
+  <!DOCTYPE html>\r\n<html lang=\"en\">\r\n  <head>\r\n    <meta charset=\"UTF-8\" />\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />\r\n    <title>Document</title>\r\n  </head>\r\n  <body></body>\r\n</html>\r\n
+  `),
+  modified2: rTabs(`
     import React from "react";
     import ReactDOM from "react-dom";
 
@@ -206,7 +41,8 @@ const examples = {
       }
 
       function toggleTheme() {
-        setTheme(theme === "light" ? "dark" : "light");
+        setTheme(theme === 'light' ? 'dark' : 'light');
+        setTheme(theme === "light" ? "light" : "light");
       }
 
       function toggleLanguage() {
@@ -505,7 +341,7 @@ const examples = {
     ## License
 
     [MIT](./LICENSE)
-  `),
+  `)
 };
 
 export default examples;
--- demo/src/sections/Content/Content.js
+++ demo/src/sections/Content/Content.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React from "react";
 
-import Paper from '@material-ui/core/Paper';
-import Divider from '@material-ui/core/Divider';
-import Typography from '@material-ui/core/Typography';
+import Paper from "@material-ui/core/Paper";
+import Divider from "@material-ui/core/Divider";
+import Typography from "@material-ui/core/Typography";
 
-import Editor from 'sections/Editor';
-import DiffEditor from 'sections/DiffEditor';
+// import Editor from 'sections/Editor';
+import DiffEditor from "sections/DiffEditor";
 
-import classNames from 'classnames';
-import useStyles from './useStyles';
+import classNames from "classnames";
+import useStyles from "./useStyles";
 
 const Content = _ => {
   const classes = useStyles();
 
   return (
-    <Paper elevation={0} square={true} className={classNames('full-size', classes.root)}>
+    <Paper
+      elevation={0}
+      square={true}
+      className={classNames("full-size", classes.root)}
+    >
       <div className={classes.editor}>
-        <Typography variant="h4" className={classes.title}>Editor</Typography>
+        <Typography variant="h4" className={classes.title}>
+          Editor
+        </Typography>
         <Divider />
-        <Editor />
+        {/* <Editor /> */}
       </div>
       <div className={classes.diffEditor}>
-        <Typography variant="h4" className={classes.title}>Diff Editor</Typography>
+        <Typography variant="h4" className={classes.title}>
+          Diff Editor
+        </Typography>
         <Divider />
         <DiffEditor />
       </div>
     </Paper>
   );
-}
+};
 
 export default Content;
